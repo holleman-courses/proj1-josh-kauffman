d98112a435d8dd60d3396b93cf6802bbd60dbfd6
jkauffm4 <jkauffm4@charlotte.edu>
2025-03-13 09:47:04 -0400

    Update train.py

--- training/train.py
+++ training/train.py
@@ -30,7 +30,7 @@ def build_model():
   x = tf.keras.layers.MaxPool2D(pool_size = (2,2), strides = (2,2))(res_con_2)
   x = tf.keras.layers.Flatten()(x)
   x = tf.keras.layers.Dense(128)(x)
-  outputs = tf.keras.layers.Dense(10)(x)
+  outputs = tf.keras.layers.Dense(2)(x)
 
   model = tf.keras.Model(inputs, outputs) # Add code to define model 1.
   return model
